44971d08ceacc12d3c7dd2455cf691237e48f8a0
Matti Lehtinen <matlehti@gmail.com>
2015-02-20 21:54:15 +0200

    Add very simple controlling logic

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
-var net = require("net");
-var util = require('util');
-var JSONStream = require('JSONStream');
+var net = require("net"),
+    util = require('util'),
+    crypto = require('crypto'),
+    JSONStream = require('JSONStream'),
+    _ = require('lodash');
 
 var serverHost = process.argv[2];
 var serverPort = process.argv[3];
-var name = process.argv[4];
+var name = process.argv[4] + crypto.randomBytes(2).toString('hex');
+
+var width = null,
+    height = null,
+    playerNo = null;
 
 console.log("I'm", name, "and connect to", serverHost + ":" + serverPort);
 
@@ -20,7 +26,9 @@ client = net.connect(serverPort, serverHost, function() {
 });
 
 function send(json) {
-    return client.write(JSON.stringify(json));
+    var jsonString = JSON.stringify(json);
+    console.log("SEND: " + jsonString);
+    return client.write(jsonString);
 }
 
 jsonStream = client.pipe(JSONStream.parse());
@@ -33,9 +41,23 @@ jsonStream.on('data', function(data) {
     } else if (data.msg === 'created') {
         console.log('Game created');
     } else if (data.msg === 'start') {
-        console.log('Game started');
-    } else if (data.msg === 'gameEnd') {
+        console.log('Game start');
+        width = data.data.level.width;
+        height = data.data.level.height;
+        playerNo = _.findIndex(data.data.players, {name: name});
+        console.log("Width: " + width + ", height: " + height + ", I'm player " + (playerNo + 1));
+    } else if (data.msg === 'end') {
         console.log('Game ended');
+    } else if (data.msg === 'positions') {
+        var snake = data.data[playerNo];
+        var x = snake.body[0][0];
+        var y = snake.body[0][1];
+        var direction = snake.direction;
+        console.log(x + ", " + y + " | " + direction);
+        if(x <= 5 && direction == 3) send({msg: 'control', data: {direction: 1}});
+        if(x >= width-6 && direction == 4) send({msg: 'control', data: {direction: 2}});
+        if(y <= 6 && direction == 1) send({msg: 'control', data: {direction: 4}});
+        if(y >= height-6 && direction == 2) send({msg: 'control', data: {direction: 3}});
     }
 });
 
